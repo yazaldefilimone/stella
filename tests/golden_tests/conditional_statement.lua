60dbfbca55dd2efc007087181cf4ffa020161607
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-27 14:27:31 +0200

    fix: if stmt parser error, #4

--- tests/golden_tests/conditional_statement.lua
+++ tests/golden_tests/conditional_statement.lua
@@ -1,7 +1,20 @@
-local age: number = 25
+-- local age: number = 25
 
-if age >= 18 then
-    print("You are an adult.")
-else
-    print("You are a minor.")
+-- if age >= 18 then
+--     print("You are an adult.")
+-- else
+--     print("You are a minor.")
+-- end
+
+
+function test(t: boolean)
+  if t then
+    return 10
+  else
+    return "Hello"
+  end
 end
+
+
+local result: number = test(true)
+local n: string = test(false)
