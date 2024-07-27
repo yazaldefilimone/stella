42e259224283efe87c75fcb5b0a7b79713f40f79
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-07-27 13:33:55 +0200

    empty stmt, #4

--- src/checker/check_empty_statement.rs
+++ src/checker/check_empty_statement.rs
@@ -1,8 +1,8 @@
 use super::Checker;
-use crate::ast::ast;
+use crate::{ast::ast, diagnostics::Diagnostic, types::Type};
 
 impl Checker {
-  pub fn check_empty_statement(&mut self, _empty: &ast::EmptyStatement) {
-    // Empty statements don't change the type context
+  pub fn check_empty_statement(&mut self, _empty: &ast::EmptyStatement) -> Result<Type, Diagnostic> {
+    Ok(Type::Nil)
   }
 }
